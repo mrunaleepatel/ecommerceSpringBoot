0040b6d3de77790de594032876afdc8c998def7e
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-10-12 20:57:04 -0500

    has errors

--- src/main/java/com/onlineshopping/springboot/configuration/JwtRequestFilter.java
+++ src/main/java/com/onlineshopping/springboot/configuration/JwtRequestFilter.java
@@ -1,4 +1,70 @@
 package com.onlineshopping.springboot.configuration;
 
-public class JwtRequestFilter {
+import com.onlineshopping.springboot.service.JwtService;
+import com.onlineshopping.springboot.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter implements Filter {
+    public static String CURRENT_USER = "";
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+        if (requestTokenHeader!= null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                username = jwtUtil.getUsernameFromToken(jwtToken);
+                CURRENT_USER = username;
+            } catch (IllegalAccessException e) {
+                System.out.println("Unable to get JWT Token");
+            } catch (ExpiredJwtException e){
+                System.out.println("Expired JWT Token");
+            }
+        } else {
+            System.out.println("JWT Token does not begin with Bearer");
+        }
+        if (username!= null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = jwtService.getUserDetails(username);
+            if (jwtUtil.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+
+    }
 }
